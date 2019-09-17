b855dd766834b03deddc874c927c61b8b0c1f520
1894423 <bhakti.savani33@gmail.com>
2019-09-17 17:09:33 -0400

    new commit

--- app/src/main/java/com/example/authenticationfirebase/authenticationSuccess.java
+++ app/src/main/java/com/example/authenticationfirebase/authenticationSuccess.java
@@ -4,16 +4,34 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class authenticationSuccess extends Fragment {
 
+    TextView name;
+    Button logout;
+    FirebaseFirestore db;
+    FirebaseUser user;
 
     public authenticationSuccess() {
         // Required empty public constructor
@@ -24,7 +42,11 @@ public class authenticationSuccess extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-           }
+
+        if(getArguments()!=null)
+            user=getArguments().getParcelable("user");
+        db=FirebaseFirestore.getInstance();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,4 +55,45 @@ public class authenticationSuccess extends Fragment {
         return inflater.inflate(R.layout.fragment_authentication_success, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        name=view.findViewById(R.id.authName);
+        logout=view.findViewById(R.id.logout);
+        //delete=view.findViewById(R.id.delete);
+
+
+        name.setText(  "user logged in");
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                NavController navController= Navigation.findNavController(getActivity(),R.id.host_fragment);
+                navController.navigate(R.id.homescreen);
+            }
+        });
+        Toast.makeText(getActivity(),"name:"+name,Toast.LENGTH_LONG).show();
+        //readFireStore();
+    }
+
+    public void readFireStore(){
+        DocumentReference documentReference=db.collection("users").document(user.getUid());
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot snap=task.getResult();
+                    if (snap.exists()){
+                        System.out.println(snap.getData());
+                        name.setText("Welcome "+snap.get("Name") );
+                        Toast.makeText(getActivity(),"name:"+name,Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+
+    }
 }
